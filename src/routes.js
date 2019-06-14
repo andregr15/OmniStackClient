5dc961daf697fb94c46aaaa4a937cefaf43e8d37
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-06-14 10:49:40 -0300

    Adding BrowserRouter

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,8 @@ import New from './pages/New';
 function Routes() {
   return (
     <Switch>
-      <Route path="/" exact Component={Feed}/>
-      <Route path="/new" Component={New}/>
+      <Route path="/" exact component={Feed}/>
+      <Route path="/new" component={New}/>
     </Switch>
   );
 }
