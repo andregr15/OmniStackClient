bf136b16acbd659a015c87d3eed12f9c95fda803
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-06-14 12:01:58 -0300

    Adding socket.io-client to post and like socket messages

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 
@@ -14,9 +15,29 @@ class Feed extends Component {
   };
 
   async componentDidMount() {
+    this.registerToSocket();
+
     const response = await api.get('posts');
 
     this.setState({ feed: response.data });
+  }
+
+  registerToSocket = () => {
+    const socket = io('http://localhost:3333');
+
+    // post, like
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on('like', likedPost => {
+      this.setState({ 
+        feed: this.state.feed.map(post =>
+            post._id === likedPost._id ? likedPost : post
+          )
+      });
+    });
   }
 
   handleLike = id => {
@@ -35,19 +56,19 @@ class Feed extends Component {
                   <span className="place">{post.place}</span>
                 </div>
 
-                <img src={more} alt="Mais"/>
+                <img src={more} alt="Mais" />
               </header>
 
-              <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
+              <img src={`http://localhost:3333/files/${post.image}`} alt="" />
 
               <footer>
                 <div className="actions">
                   <button type="button" onClick={() => this.handleLike(post._id)}>
-                    <img src={like} alt="Like"/>
+                    <img src={like} alt="Like" />
                   </button>
-                  
-                  <img src={comment} alt="Comentar"/>
-                  <img src={send} alt="Enviar"/>
+
+                  <img src={comment} alt="Comentar" />
+                  <img src={send} alt="Enviar" />
                 </div>
 
                 <strong>{post.likes} curtidas</strong>
