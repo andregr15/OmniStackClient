91600dcdc876d17d4638a0e91d4bebab2987284e
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-06-14 11:27:29 -0300

    Adding axios and fetch posts

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import './Feed.css';
 
@@ -8,36 +9,50 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+  state = {
+    feed: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get('posts');
+
+    this.setState({ feed: response.data });
+  }
+
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>User Name</span>
-              <span className="place">Local</span>
-            </div>
+        {
+          this.state.feed.map(post => (
+            <article>
+              <header>
+                <div className="user-info">
+                  <span>{post.author}</span>
+                  <span className="place">{post.place}</span>
+                </div>
 
-            <img src={more} alt="Mais"/>
-          </header>
+                <img src={more} alt="Mais"/>
+              </header>
 
-          <img src="" alt=""/>
+              <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
 
-          <footer>
-            <div className="actions">
-              <img src={like} alt="Like"/>
-              <img src={comment} alt="Comentar"/>
-              <img src={send} alt="Enviar"/>
-            </div>
+              <footer>
+                <div className="actions">
+                  <img src={like} alt="Like"/>
+                  <img src={comment} alt="Comentar"/>
+                  <img src={send} alt="Enviar"/>
+                </div>
 
-            <strong>likes</strong>
+                <strong>{post.likes} curtidas</strong>
 
-            <p>
-              description
-              <span>hashtags</span>
-            </p>
-          </footer>
-        </article>
+                <p>
+                  {post.description}
+                  <span>{post.hashtags}</span>
+                </p>
+              </footer>
+            </article>
+          ))
+        }
       </section>
     );
   }
